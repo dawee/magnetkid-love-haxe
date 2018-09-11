bfcff735683d67d8ce3c424edcf05f008423a367
David Corticchiato <david.corticchiato@gmail.com>
2018-09-11 18:30:28 -0400

    restart fresh

--- src/magnetkid/Game.hx
+++ src/magnetkid/Game.hx
@@ -4,14 +4,7 @@ import love.JoystickEventListener;
 import love.LifecycleListener;
 import love.Love;
 
-using magnetkid.component.Kid;
-using magnetkid.component.Platform;
-using magnetkid.physics.World;
-
 class Game implements LifecycleListener implements JoystickEventListener {
-  private var world:World;
-  private var camera:Camera;
-  private var kid:Kid;
 
   static public function main():Void {
     var game = new Game();
@@ -20,58 +13,20 @@ class Game implements LifecycleListener implements JoystickEventListener {
     Love.bridge.addJoystickEventListener(game);
   }
 
-  public function new() {
-    kid = new Kid();
-    world = new World();
-    camera = new Camera(Kid.HEIGHT / World.KID_HEIGHT);
+  public function new() {}
 
-    world.addIntentListener(kid);
-  }
+  public function load() {}
 
-  public function load() {
-    kid.load();
-    Love.graphics.setBackgroundColor(0.23, 0.03, 0.32, 1.0);
-  }
+  public function draw() {}
 
-  public function draw() {
-    kid.draw(camera, world.kid);
-    world.platforms.map(platform -> Platform.draw(camera, platform));
-  }
+  public function update(dt: Float) {}
 
-  public function update(dt: Float) {
-    world.step(dt);
-    camera.lookAt(world.kid.position);
-  }
+  public function gamepadaxis(axis: String, value: Float) {}
 
-  public function gamepadaxis(axis: String, value: Float) {
-    switch (axis) {
-      case 'leftx': {
-        if (value < -0.25) {
-          world.startWalkingLeft();
-        } else if (value > 0.25) {
-          world.startWalkingRight();
-        } else {
-          world.stopWalkingLeft();
-          world.stopWalkingRight();
-        }
-      }
-    }
-  }
+  public function gamepadpressed(button: String) {}
 
-  public function gamepadpressed(button: String) {
-    switch (button) {
-      case 'a': world.kidJump();
-      case 'rightshoulder': world.seekAttractionUp();
-    }
-  }
+  public function gamepadreleased(button: String) {}
 
-  public function gamepadreleased(button: String) {
-    switch (button) {
-      case 'rightshoulder': world.stopSeekingAttractionUp();
-    }
-  }
-
-  public function joystickaxis(axis: Int, value: Float) {
-  }
+  public function joystickaxis(axis: Int, value: Float) {}
 
 }
--- src/magnetkid/physics/IntentListener.hx
+++
@@ -1,9 +0,0 @@
-package magnetkid.physics;
-
-using World.IntentName;
-
-interface IntentListener {
-  public function validatedIntent(name: IntentName):Void;
-  public function addedIntent(name: IntentName):Void;
-  public function removedIntent(name: IntentName):Void;
-}
